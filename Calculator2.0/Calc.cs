d6abf3002e809873ca58020183bdff92c7b07fe3
Allkashi <mail50025@gmail.com>
2022-01-10 19:44:18 +0500

    calcFin

---
+++ Calculator2.0/Calc.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace Calculator2._0
+{
+    class Calc
+    {
+
+        public static string Calculator(string inp)
+        {
+            DataTable dt = new DataTable();
+            return dt.Compute(inp, "").ToString();
+        }
+
+        public static void SaveFile(string inp)
+        {
+            string writePath = @"C:\Users\rapun\source\repos\Calculator2.0\TextHistory.txt";
+
+            using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(inp);
+            }
+
+        }
+
+        public static void ReadFromDB(ObservableCollection<string> history)
+        {
+            using (var connection = new System.Data.SQLite.SQLiteConnection("Data Source = C:\\Users\\rapun\\OneDrive\\Desktop\\SQLiteStudio\\calc.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "select * from list";
+                var reader = command.ExecuteReader();
+                while (reader.Read() && history.Count < 10)
+                {
+                    history.Add(System.Convert.ToString(reader["expression"]));
+                }
+            }
+        }
+
+
+        public static void LoadToDB(string inp)
+        {
+            using (var connection = new System.Data.SQLite.SQLiteConnection("Data Source =  C:\\Users\\rapun\\OneDrive\\Desktop\\SQLiteStudio\\calc.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "insert into list (expression) values(" + "\'" + inp + "\'" + ")";
+                var reader = command.ExecuteReader();
+            }
+
+
+        }
+
+
+    }
+
+
+}
---
+++ Calculator2.0/MainViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace Calculator2._0
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+		private string _Count;
+		private bool _isError;
+		public ObservableCollection<string> history { get; set; }
+
+		public ICommand Calculate { get; }
+		public ICommand Erase { get; }
+		public ICommand Delete { get; }
+		public ICommand GetButtonValue { get; }
+
+
+		private string _selectedText;
+
+		public string SelectedText
+		{
+			get { return _selectedText; }
+			set {
+				_selectedText = value;
+				OnPropertyChanged(nameof(SelectedText));
+			}
+		}
+
+
+		public string Count
+		{
+			get => _Count;
+			set
+			{
+				_Count = value;
+				OnPropertyChanged(nameof(Count));
+			}
+		}
+
+		public bool IsError
+		{
+			get { return _isError; }
+			set
+			{
+				_isError = value;
+				OnPropertyChanged(nameof(IsError));
+			}
+		}
+
+
+		public MainViewModel()
+		{
+			history = new ObservableCollection<string>();
+
+			Calc.ReadFromDB(history);
+
+			Calculate = new RelayCommand<string>(x =>
+			{
+				string str = Count;
+
+				if (history.Count < 10)
+				{
+					history.Insert(0, Count);
+				}
+				else
+				{
+					history.RemoveAt(9);
+					history.Insert(0, Count);
+				}
+
+				Calc.SaveFile(Count);
+				Calc.LoadToDB(Count);
+
+				try
+				{
+					Count = Calc.Calculator(Count.Replace(',', '.'));
+					str += "=" + Count;
+
+				}
+				catch
+				{
+					Count = "Ошибка в выражении";
+					IsError = true;
+				}
+			});
+
+			Erase = new RelayCommand<string>(x =>
+			{
+				if (IsError || String.IsNullOrEmpty(Count))
+					return;
+				Count = Count.Remove(Count.Length - 1);
+			});
+
+
+			Delete = new RelayCommand<string>(x =>
+			{
+				Count = "";
+				IsError = false;
+
+			});
+
+
+			GetButtonValue = new RelayCommand<string>(x => 
+			{
+				Count += x;
+
+			});
+
+
+		}
+		
+
+
+	}
+}
